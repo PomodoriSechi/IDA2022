ebdcc9ad8faf8215a371abc8fcbff07de11c825b
balz <balz.schlegel@gmail.com>
2022-05-05 13:14:32 +0200

    commit

--- tag11/assets/script.js
+++ tag11/assets/script.js
@@ -1,33 +1,21 @@
- let n = 0; /*schlüsselwort let und variablenname. zuweisung des werts von rechts nach links */
- function buttonRight() {
-     n++; /* ist dasselbe wie n=n+1*/
-     console.log('pressed:' + n)
-     document.getElementById("element").style.left = n + "5px";
-
-}
- function buttonLeft() {
-     n++; /* ist dasselbe wie n=n+1*/
-     console.log('pressed:' + n)
-     document.getElementById("element").style.left = n + "-" +"5px";
-
-}
- function buttonUp() {
-     n++; /* ist dasselbe wie n=n+1*/
-     console.log('pressed:' + n)
-     document.getElementById("element").style.top =  n + "-" +"5px";
-
-}
- function buttonDown() {
-     n++; /* ist dasselbe wie n=n+1*/
-     console.log('pressed:' + n)
-     document.getElementById("element").style.top = n + "5px";
-
+function change() {
+    let snow = document.getElementsByClassName("snowflake");
+    snow.style.left = Math.random() * window.innerWidth + "px";
 }
 
-function neuesElement() {
-    let button = document.createElement("button");
-    button.classList.add('specialbutton');
-    button.style.left = Math.random() * window.innerWidth + "px";
-    button.style.top = Math.random() * window.innerHeight + "px";
-    document.body.appendChild(button);
+function newElement() {
+    let div = document.createElement("div");
+    div.classList.add('snowflake');
+    div.style.left = Math.random() * window.innerWidth + "px";
+    div.style.top = Math.random() * window.innerHeight + "px";
+    document.body.appendChild(div);
+    n++; /* ist dasselbe wie n=n+1*/
+    console.log('pressed:' + n)
+    document.getElementById("counter").innerHTML = n
 }
+let n = 0; /*schlüsselwort let und variablenname. zuweisung des werts von rechts nach links */
+function buttonPressed() {
+    n++; /* ist dasselbe wie n=n+1*/
+    console.log('pressed:' + n)
+}
+
