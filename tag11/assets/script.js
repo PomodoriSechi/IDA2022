21a81d88df4c8961f670565a133bd4d9c0a5d210
balz <balz.schlegel@gmail.com>
2022-05-05 16:09:59 +0200

    lol

--- tag11/assets/script.js
+++ tag11/assets/script.js
@@ -1,18 +1,21 @@
-function change() {
-    let snow = document.getElementsByClassName("snowflake");
-    snow.style.left = Math.random() * window.innerWidth + "px";
-}
+
 
 function newElement() {
-    let div = document.createElement("div");
-    div.classList.add('snowflake');
-    div.style.left = Math.random() * window.innerWidth + "px";
-    div.style.top = Math.random() * window.innerHeight + "px";
-    document.body.appendChild(div);
+    let snow = document.createElement("div");
+    snow.classList.add('snowflake');
+    snow.style.left = Math.random() * window.innerWidth + "px";
+    snow.style.top = Math.random() * window.innerHeight + "px";
+    document.body.appendChild(snow);
     n++; /* ist dasselbe wie n=n+1*/
     console.log('pressed:' + n)
     document.getElementById("counter").innerHTML = n
 }
+
+function change() {
+    let snow = document.getElementsByClassName("snowflake");
+    div.style.left = 5 + "px";
+}
+
 let n = 0; /*schlüsselwort let und variablenname. zuweisung des werts von rechts nach links */
 function buttonPressed() {
     n++; /* ist dasselbe wie n=n+1*/
