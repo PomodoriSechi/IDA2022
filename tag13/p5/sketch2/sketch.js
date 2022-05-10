47f0bfbb9cdf6515f092a8fd105c5f04253b5e3f
balz <balz.schlegel@gmail.com>
2022-05-10 13:11:16 +0200

    ok

--- tag13/p5/sketch2/sketch.js
+++ tag13/p5/sketch2/sketch.js
@@ -4,16 +4,30 @@ let count = 0;
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     let url = "chrome_history.json";
-    history = loadJSON(url);
-    //Zugriff auf Eigenschaften
-    console.log(history[0].title); //Gibt undefined aus, die Daten sind noch nicht geladen
+    loadJSON(url, loaded);
+    background(130);
+    fill(0);
+    textSize(30)
+    textAlign(CENTER, CENTER);
+    frameRate(1);
+}
+function loaded(data){
+    history=data;
+}
+function draw() {
+
+  background(255, 200);
+    text(history[count].title, width / 2, height / 2);
+    count++;
+    if (count > Object.keys(history).length) {
+        count = 0;
+    }
 }
 
-function draw() {
-  background(255, 50);
-  strokeWeight(0);
-  ellipse(mouseX, mouseY, 50, 50);
-  // Grayscale integer value
-  fill(255, 204, 0);
-
+function keyReleased() {
+  if (key == 's' || key == 'S') {
+      let d = new Date();
+      let now = d.getFullYear() + "" + (d.getMonth() + 1) + "" + d.getDate() + "" + (d.getHours() + 1) + "-" + (d.getMinutes() + 1) + "" + (d.getSeconds() + 1) + "-" + frameCount;
+      saveCanvas(now, 'png');
+  }
 }
---
+++ tag13/p5/sketch3/sketch.js
@@ -0,0 +1,14 @@
+  function setup() {
+    createCanvas(1080, 640);
+    background(255);
+}
+
+
+function draw() {
+  background(255, 50);
+  strokeWeight(0);
+  ellipse(mouseX, mouseY, 50, 50);
+  // Grayscale integer value
+  fill(255, 204, 0);
+
+}
